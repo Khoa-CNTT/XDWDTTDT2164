380887973d9d4061994eb8f8e4354d2f1c9b9192
Adalheida <hoangquyktm@gmail.com>
2025-03-31 12:55:05 +0700

    update FE code 1.0

---
+++ frontend/src/components/common/BotSuperio.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="page-footer">
+    <p>© 2025 KLTN - Trường Đại học Duy Tân</p>
+  </footer>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,42 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "@/views/admin/dashboard/index_2.vue"; // Trang chứa layout chính
 
 const routes = [
   {
-    path: "/",
-    component: () => import("@/views/Home.vue"),
-  },
-  {
-    path: "/about",
-    component: () => import("@/views/About.vue"),
+    //Login
+    // path: "/login-signup",
+    // component: ()=> import("@/views/common/Login.vue"),
+
+
+    path: "/admin-dashboard",
+    // component: Dashboard, // Luôn hiển thị Dashboard.vue
+    children: [
+      {
+        path: "", // Mặc định khi vào /admin-dashboard
+        redirect: "admin-dashboard", // Chuyển hướng sang trang con mặc định
+      },
+      {
+        path: "user-management", 
+        component: () => import("@/views/admin/dashboard/User-management.vue"),
+      },
+      
+      {
+        path: "company-management",
+        component: () => import("@/views/admin/dashboard/Company-management.vue"),
+      },
+      {
+        path: "post-management",
+        component: () => import("@/views/admin/dashboard/Post-management.vue"),
+      },
+      {
+        path: "list-jobtype",
+        component: () => import("../views/admin/dashboard/List-jobtype.vue"),
+      },
+      {
+        path: "list-skills",
+        component: () => import("../views/admin/dashboard/List-skills.vue"),
+      },
+    ],
   },
 ];
 
